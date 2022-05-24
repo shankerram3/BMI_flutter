fe685ca08a20d3aa27e4bd96a167f51310d06dfc
shankerram3 <shankerram3@gmail.com>
2022-05-24 17:13:53 +0530

    frst commit

---
+++ lib/login_page.dart
@@ -0,0 +1,112 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class LoginPage extends StatefulWidget {
+  // ignore: prefer_const_constructors_in_immutables
+  LoginPage({Key? key}) : super(key: key);
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color.fromARGB(255, 234, 236, 229),
+      body: SafeArea(
+        child: Center(
+          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+            Text(
+              'Hello Again!',
+              style: GoogleFonts.bebasNeue(
+                fontSize: 52,
+              ),
+            ),
+            SizedBox(
+              height: 50,
+            ),
+            Text(
+              'Welcome back ,you\'ve been missed',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 18,
+              ),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            // ignore: avoid_unnecessary_containers
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25.0),
+              child: Container(
+                decoration: BoxDecoration(
+                    color: Color.fromARGB(255, 255, 248, 248),
+                    border: Border.all(color: Color.fromARGB(255, 0, 0, 0)),
+                    borderRadius: BorderRadius.circular(12)),
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 20.0),
+                  child: TextField(
+                    decoration: InputDecoration(
+                        border: InputBorder.none, hintText: 'Email'),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 25.0,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25.0),
+              child: Container(
+                decoration: BoxDecoration(
+                    color: Color.fromARGB(255, 255, 248, 248),
+                    border: Border.all(color: Color.fromARGB(255, 0, 0, 0)),
+                    borderRadius: BorderRadius.circular(12)),
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 20.0),
+                  child: TextField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                        border: InputBorder.none, hintText: 'password'),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(height: 25.0),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25.0),
+              child: Container(
+                padding: EdgeInsets.all(20),
+                decoration: BoxDecoration(
+                    color: Colors.deepPurple,
+                    borderRadius: BorderRadius.circular(12)),
+                child: Center(
+                  child: Text(
+                    'Sign In',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(height: 25.0),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: const [
+                Text('Not a member?'),
+                Text('  Signup!',
+                    style: TextStyle(
+                      color: Colors.blue,
+                      fontWeight: FontWeight.bold,
+                    ))
+              ],
+            )
+          ]),
+        ),
+      ),
+    );
+  }
+}
